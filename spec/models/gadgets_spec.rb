ad6b71f684bc5da30ac5534ec7de488ac5da2d23
shekhardokania <shekhar.pesit@gmail.com>
2014-10-15 00:43:09 +0530

    added test for gadget search

--- spec/models/gadgets_spec.rb
+++ spec/models/gadgets_spec.rb
@@ -1,4 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Gadget, :type => :model do
+  describe 'search using name or desc'
+    it 'find by name' do
+      Gadget.create(name: "sony xperia",description: "smartphone",user: @user)
+      gadgets = Gadget.search('sony')
+      expect(gadgets.count).to eq(1)
+    end
+  it 'find by desc' do
+    Gadget.create(name: "sony xperia",description: "smartphone",user: @user)
+    gadgets = Gadget.search('phone')
+    expect(gadgets.count).to eq(1)
+  end
 end
