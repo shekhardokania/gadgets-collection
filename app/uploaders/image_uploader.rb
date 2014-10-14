10f2a81776584fd3ef99186ac565cc10e7ea93a2
shekhardokania <shekhar.pesit@gmail.com>
2014-10-15 00:18:58 +0530

    added few refactoings

---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,17 @@
+class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::RMagick
+
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  version :thumb do
+    process resize_to_limit: [200, 150]
+  end
+
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+end
