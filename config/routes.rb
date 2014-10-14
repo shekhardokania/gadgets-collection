ae4015037a20cb3686a446de29d1d1cedd31820e
shekhardokania <shekhar.pesit@gmail.com>
2014-10-14 21:28:25 +0530

    home index page with user signup and signip

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    delete '/sign_out', to: 'devise/sessions#destroy', as: 'sign_out'
+    get '/sign_in', to: 'devise/sessions#new', as: 'sign_in'
+    get '/sign_up', to: 'devise/registrations#new', as: 'sign_up'
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
+  resources :gadgets
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
